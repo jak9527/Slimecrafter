7d755b6b34d8133958deb04fda2d065fc59eb6a6
Jacob Karvelis <jak9527@rit.edu>
2023-12-08 17:21:39 -0500

    added piston sound to jumpamatron

--- src/main/java/com/slimecrafter/item/Jumpamatron.java
+++ src/main/java/com/slimecrafter/item/Jumpamatron.java
@@ -24,6 +24,7 @@ public class Jumpamatron extends Item {
         playerEntity.setVelocity(playerEntity.getVelocity().
                 add(playerEntity.getRotationVec(0).
                         multiply(new Vec3d(ratio*-0.7, ratio*-1, ratio*-0.7))));
+        playerEntity.playSound(SoundEvents.BLOCK_PISTON_EXTEND, SoundCategory.PLAYERS, 1.0f, 1.0f);
         context.getWorld().playSound(playerEntity, posClicked, SoundEvents.ENTITY_SLIME_JUMP, SoundCategory.PLAYERS, 1.0f, 1.0f);
         playerEntity.getItemCooldownManager().set(this, 20);
 
