2eccefd1c623e32944f4e66fa98fd154339f5396
Jacob Karvelis <jak9527@rit.edu>
2023-12-08 17:44:49 -0500

    added model generator

--- src/main/java/com/slimecrafter/datagen/ModModelProvider.java
+++ src/main/java/com/slimecrafter/datagen/ModModelProvider.java
@@ -1,4 +1,29 @@
 package com.slimecrafter.datagen;
 
-public class ModModelProvider {
+import com.slimecrafter.block.ModBlocks;
+import com.slimecrafter.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+
+public class ModModelProvider extends FabricModelProvider {
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.DEEPSLATE_SLIMIUM_ORE_BLOCK);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SLIMIUM_ORE_BLOCK);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SLIMIUM_BLOCK);
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        itemModelGenerator.register(ModItems.RAW_SLIMIUM, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SLIMIUM_INGOT, Models.GENERATED);
+        itemModelGenerator.register(ModItems.JUMPAMATRON, Models.GENERATED);
+    }
 }
