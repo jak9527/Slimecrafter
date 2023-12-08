7f1b31c4f02f84b4233577a27c89a9f8762cd149
Jacob Karvelis <jak9527@rit.edu>
2023-12-08 17:18:29 -0500

    data generator job runs ok. Gonna follow the tut and see if it works

---
+++ src/main/java/com/slimecrafter/datagen/SlimecrafterDataGenerator.java
@@ -0,0 +1,17 @@
+package com.slimecrafter.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SlimecrafterDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator generator) {
+        FabricDataGenerator.Pack pack = generator.createPack();
+
+        // Adding a provider example:
+        //
+        // pack.addProvider(AdvancementsProvider::new);
+    }
+
+}
