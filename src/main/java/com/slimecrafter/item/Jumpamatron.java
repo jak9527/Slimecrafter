59db15c8bb6e28324206b08d9dff64802a5887ad
Jacob Karvelis <jak9527@rit.edu>
2023-12-08 20:46:23 -0500

    changed texture, added push mobs

--- src/main/java/com/slimecrafter/item/Jumpamatron.java
+++ src/main/java/com/slimecrafter/item/Jumpamatron.java
@@ -1,17 +1,37 @@
 package com.slimecrafter.item;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
 public class Jumpamatron extends Item {
     public Jumpamatron(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+        double ratio = 2/calcDistance(user.getPos(), entity.getPos());
+        ratio = Math.min(ratio, 1);
+        entity.setVelocity(entity.getVelocity()
+                .add(user.getRotationVec(0)
+                        .multiply(new Vec3d(ratio*-1.2, ratio*-1, ratio*-1.2))));
+        user.playSound(SoundEvents.BLOCK_PISTON_EXTEND, SoundCategory.PLAYERS, 1.0f, 1.0f);
+        entity.playSound(SoundEvents.ENTITY_SLIME_JUMP, 1.0f, 1.0f);
+        user.getItemCooldownManager().set(this, 20);
+
+        stack.damage(1, user,
+                playerEntity1 -> playerEntity1.sendToolBreakStatus(playerEntity1.getActiveHand()));
+
+        return ActionResult.SUCCESS;
     }
 
     @Override
